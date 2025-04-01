c02e38e0bb152d813a5b24332ed36cfbf58af9dd
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-01 07:54:46 +0530

    Add new services and update build.gradle

---
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -0,0 +1,30 @@
+package ticket.booking.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class UserBookingService {
+    private User user;
+
+    // variable userList of type
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    // constructor
+    public UserBookingService(User user) throws IOException {
+        // throws ioException is used to handle scenarios where file is not found or path to the file is wrong
+        this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        }); // assigning userList variable to json Data
+
+    }
+}
