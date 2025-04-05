4b5c561e3d33883e361010c9f60f16a4060ac47c
Rahul Kashyap <rahulkashyap4019@gmail.com>
2025-04-05 08:41:40 +0530

    Add getter and setter methods to User class

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -8,6 +8,64 @@ public class User {
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
+
+    // constructor
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId) {
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userId = userId;
+
+    }
+
+    // another constructor if nothing passed
+    public User() {
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+
+    public void printTickets() {
+        for (int i = 0; i < ticketsBooked.size(); i++) {
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    // Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
 
 
